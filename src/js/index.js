3ec0ff0365b27eb5290f97ed78224b25b367fa3b
demetrioverba <65371371+demetrioverba@users.noreply.github.com>
2020-11-25 17:41:51 +0200

    2.3

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,14 @@
-window.onscroll = function () { myFunction() };
 
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
+// var header = document.getElementById("myHeader");
 
-function myFunction() {
-    if (window.pageYOffset > sticky) {
+// function myFunction() {
+//     if (window.pageYOffset > sticky) {
 
-        header.classList.remove("header");
-        header.classList.add("sticky");
-    } else {
-        header.classList.add("header");
-        header.classList.remove("sticky");
+//         header.classList.remove("header");
+//         header.classList.add("sticky");
+//     } else {
+//         header.classList.add("header");
+//         header.classList.remove("sticky");
 
-    }
-}
+//     }
+// }
